9742c34b601e48000637d13d3adfab5005b8adaa
Ilya Chvanov <mercy1ess@list.ru>
2021-08-19 17:00:17 +0300

    Fix problem input-radio, .js file

--- js/app.js
+++ js/app.js
@@ -3,13 +3,13 @@ function swap(value) {
     var c2 = document.getElementById('review2')
     var c3 = document.getElementById('review3')
     if (value == '1') {
-        c1.style.display = 'block'
-        c2.style.display = 'none'
+        c1.style.display = 'none'
+        c2.style.display = 'block'
         c3.style.display = 'none'
     }
     if (value == '2') {
-        c1.style.display = 'none'
-        c2.style.display = 'block'
+        c1.style.display = 'block'
+        c2.style.display = 'none'
         c3.style.display = 'none'
     }
     if (value == '3') {
